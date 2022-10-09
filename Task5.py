ec5ef8a6c6c0783c58f0f7c4dabc3d2100f974a1
Alexey <pwz-and_10@mail.ru>
2022-10-09 16:25:36 +0600

    Create Task5

--- Task5.py
+++ Task5.py
@@ -1,67 +1,116 @@
 # Даны два файла, в каждом из которых находится запись многочлена. 
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-import re
-import itertools
+import random
 
-file1 = 'Polynomial.txt'
-file2 = 'Polynomial2.txt'
-file_sum = 'PolynomialSum.txt'
+# запись в файл
+def write_file(name,st):
+    with open(name, 'w') as data:
+        data.write(st)
 
-def read_pol(file):
-    with open(str(file), 'r') as data:
-        pol = data.read()
-    return pol
+# создание случайного числа от 0 до 100
+def rnd():
+    return random.randint(0,101)
 
-def convert_pol(pol):
-    pol = pol.replace('= 0', '')
-    pol = re.sub("[*|^| ]", " ", pol).split('+')
-    pol = [char.split(' ') for char in pol]
-    pol = [[x for x in list if x] for list in pol]
-    for i in pol:
-        if i[0] == 'x': i.insert(0, 1)
-        if i[-1] == 'x': i.append(1)
-        if len(i) == 1: i.append(0)
-    pol = [tuple(int(x) for x in j if x != 'x') for j in pol]
-    return pol
+# создание коэффициентов многочлена
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+# создание многочлена в виде строки 
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0 or lst[i+2] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0 or lst[i+2] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
 
-def fold_pols(pol1, pol2):   
-    x = [0] * (max(pol1[0][1], pol2[0][1] + 1))
-    for i in pol1 + pol2:        
-        x[i[1]] += i[0]
-    res = [(x[i], i) for i in range(len(x)) if x[i] != 0]
-    res.sort(key = lambda r: r[1], reverse = True)
-    return res
+# получение степени многочлена
+def sq_mn(k):
+    if 'x^' in k:
+        i = k.find('^')
+        num = int(k[i+1:])
+    elif ('x' in k) and ('^' not in k):
+        num = 1
+    else:
+        num = -1
+    return num
 
-def get_sum_pol(pol):
-    var = ['*x^'] * len(pol)
-    coefs = [x[0] for x in pol]
-    degrees = [x[1] for x in pol]
-    new_pol = [[str(a), str(b), str(c)] for a, b, c in (zip(coefs, var, degrees))]
-    for x in new_pol:
-        if x[0] == '0': del (x[0])
-        if x[-1] == '0': del (x[-1], x[-1])
-        if len(x) > 1 and x[0] == '1' and x[1] == '*x^': del (x[0], x[0][0])
-        if len(x) > 1 and x[-1] == '1': 
-            del x[-1]
-            x[-1] = '*x'
-        x.append(' + ')
-    new_pol = list(itertools.chain(*new_pol))
-    new_pol[-1] = ' = 0'
-    return "".join(map(str, new_pol))
+# получение коэффицента члена многочлена
+def k_mn(k):
+    if 'x' in k:
+        i = k.find('x')
+        num = int(k[:i])
+    return num
 
-def write_to_file(file, pol):
-    with open(file, 'w') as data:
-        data.write(pol)
+# разбор многочлена и получение его коэффициентов
+def calc_mn(st):
+    st = st[0].replace(' ', '').split('=')
+    st = st[0].split('+')
+    lst = []
+    l = len(st)
+    k = 0
+    if sq_mn(st[-1]) == -1:
+        lst.append(int(st[-1]))
+        l -= 1
+        k = 1
+    i = 1 # степень
+    ii = l-1 # индекс
+    while ii >= 0:
+        if sq_mn(st[ii]) != -1 and sq_mn(st[ii]) == i:
+            lst.append(k_mn(st[ii]))
+            ii -= 1
+            i += 1
+        else:
+            lst.append(0)
+            i += 1
+        
+    return lst
+    
+# создание двух файлов
+k1 = int(input("Введите натуральную степень для первого файла k = "))
+k2 = int(input("Введите натуральную степень для второго файла k = "))
+koef1 = create_mn(k1)
+koef2 = create_mn(k2)
+write_file("file_1.txt", create_str(koef1))
+write_file("file_2.txt", create_str(koef2))
 
-pol1 = read_pol(file1)
-pol2 = read_pol(file2)
-pol_1 = convert_pol(pol1)
-pol_2 = convert_pol(pol2)
-
-pol_sum = get_sum_pol(fold_pols(pol_1, pol_2))
-write_to_file(file_sum, pol_sum)
-
-print(pol1)
-print(pol2)
-print(pol_sum)
+# нахождение суммы многочлена
+with open('file_1.txt', 'r') as data:
+    st1 = data.readlines()
+with open('file_2.txt', 'r') as data:
+    st2 = data.readlines()
+print(f"Первый многочлен {st1}")
+print(f"Второй многочлен {st2}")
+lst1 = calc_mn(st1)
+lst2 = calc_mn(st2)
+ll = len(lst1)
+if len(lst1) > len(lst2):
+    ll = len(lst2)
+lst_new = [lst1[i] + lst2[i] for i in range(ll)]
+if len(lst1) > len(lst2):
+    mm = len(lst1)
+    for i in range(ll,mm):
+        lst_new.append(lst1[i])
+else:
+    mm = len(lst2)
+    for i in range(ll,mm):
+        lst_new.append(lst2[i])
+write_file("file_res.txt", create_str(lst_new))
+with open('file_res.txt', 'r') as data:
+    st3 = data.readlines()
+print(f"Результирующий многочлен {st3}")
